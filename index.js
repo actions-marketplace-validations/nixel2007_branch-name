6bb2afc5e80f39248db93ddbab60fb1e8e97a1bc
Nikita Gryzlov <nixel2007@gmail.com>
2020-08-25 21:25:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 
 async function run() {
   try {
-    core.exportVariable('BRANCH_NAME', process.env.GITHUB_REF.split('/').pop());
+    core.exportVariable('BRANCH_NAME', process.env.GITHUB_REF.replace("refs/heads/", ""));
   }
   catch (error) {
     core.setFailed(error.message);
